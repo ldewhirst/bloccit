01bd1f1cada9152afcf1f5f1a0654c3244bcc899
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-11-30 06:35:07 -0500

    Checkpoint 34 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 include RandomData
 
+#Create topics
+15.times do
+  Topic.create!(
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
+    )
+end
+topics = Topic.all
+
 #Create posts
 50.times do
   Post.create!(
@@ -18,5 +27,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
