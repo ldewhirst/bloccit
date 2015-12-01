0f2d75ab63587ee843f4fd6e9689df23fc651d7d
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-01 11:51:21 -0500

    Checkpoint 36 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130045616) do
+ActiveRecord::Schema.define(version: 20151201164122) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 20151130045616) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
