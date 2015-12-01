c21a1608b3e3bb414b5dfef64b4c51a266b55db5
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-01 09:42:50 -0500

    Assignment 35 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :descripton, length: { minimum: 15 }, presence: true
+  
 end
