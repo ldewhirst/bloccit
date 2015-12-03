46efece478922d6a88a0c50f35a9564a66e91119
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-03 13:32:19 -0500

    Assignment 40 completed

--- db/migrate/20151125103006_create_comments.rb
+++ db/migrate/20151125103006_create_comments.rb
@@ -6,7 +6,5 @@ class CreateComments < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-
-    add_foreign_key :comments, :posts
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,6 +39,10 @@ RSpec.describe User, type: :model do
      it "should respond to member?" do
        expect(user).to respond_to(:member?)
      end
+
+     it "should respond to moderator?" do
+       expect(user).to respond_to(:moderator?)
+     end
    end
 
    describe "roles" do
@@ -54,6 +58,10 @@ RSpec.describe User, type: :model do
        it "should return false for #admin?" do
          expect(user.admin?).to be_falsey
        end
+
+       it "should return falso for #moderator?" do
+         expect(user.moderator?).to be_falsey
+       end
      end
 
      context "admin user" do
@@ -67,6 +75,28 @@ RSpec.describe User, type: :model do
 
        it "should return true for #admin?" do
          expect(user.admin?).to be_truthy
+       end
+
+       it "should return false for #moderator?" do
+         expect(user.moderator?).to be_falsey
+       end
+     end
+
+     context "moderator user" do
+       before do
+         user.moderator!
+       end
+
+       it "should return false for #member?" do
+         expect(user.member?).to be_falsey
+       end
+
+       it "should return false for #admin?" do
+         expect(user.admin?).to be_falsey
+       end
+
+       it "should return true for #moderator?" do
+         expect(user.moderator?).to be_truthy
        end
      end
    end
