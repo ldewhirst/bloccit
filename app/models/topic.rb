8ac87e73557ac7621e59c31a203d3d4fa72798b2
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-08 10:23:27 -0500

    assignment 46 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,7 @@ class Topic < ActiveRecord::Base
   has_many :labels, through: :labelings
 
   scope :visible_to, -> (user) { user ? all : where(public: true) }
-
+  scope :publicly_viewable, -> { visible_to.where(public: true) }
+  scope :privately_viewable, -> { visible_to.where(public: false) }
 
 end
