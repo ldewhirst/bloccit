fe24e7b026c2afdf6d7dea57349aa3c0a8af2494
Lindsay Dewhirst <lindsay.dewhirst@gmail.com>
2015-12-08 19:55:36 -0500

    Checkpoint 48 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   before_save { self.role ||= :member }
 
+  before_create :generate_auth_token
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
- validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
- validates :password, length: { minimum: 6 }, allow_blank: true
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   validates :email,
             presence: true,
@@ -36,4 +36,10 @@ class User < ActiveRecord::Base
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 
+  def generate_auth_token
+    loop do
+      self.auth_token = SecureRandom.base64(64)
+      break unless User.find_by(auth_token: auth_token)
+    end
+  end
 end
